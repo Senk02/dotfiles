7d838b5c9403a01fe3aa92fabfffc7316793375b
Senk02 <admin@senk0.com>
2026-03-18 09:41:33 -0700

    Add symlinks for batcat and fd

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -12,6 +12,16 @@ else
     exit 1
 fi
 
+# symlink batcat -> bat (debian/ubuntu naming)
+if command -v batcat &>/dev/null && ! command -v bat &>/dev/null; then
+    sudo ln -s "$(which batcat)" /usr/local/bin/bat
+fi
+
+# symlink fdfind -> fd (debian/ubuntu naming)
+if command -v fdfind &>/dev/null && ! command -v fd &>/dev/null; then
+    sudo ln -s "$(which fdfind)" /usr/local/bin/fd
+fi
+
 # starship
 if ! command -v starship &>/dev/null; then
     curl -sS https://starship.rs/install.sh | sh -s -- -y
